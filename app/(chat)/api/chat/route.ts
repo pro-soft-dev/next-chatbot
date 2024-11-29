5355ab4da7c27546a00c3bee6cbec86d5f096b2f
pro-soft-dev <soft.buddy@outlook.com>
2024-11-29 08:31:43 -0800

    fix `'Promise<Response | undefined>' is not assignable to type 'void | Promise<void>'.`

--- app/(chat)/api/chat/route.ts
+++ app/(chat)/api/chat/route.ts
@@ -79,7 +79,7 @@ export async function POST(request: Request) {
     system: systemPrompt,
     messages: coreMessages,
     maxSteps: 5,
-    onFinish: async ({ responseMessages: Array<CoreToolMessage | CoreAssistantMessage> }) => {
+    onFinish: async ({ responseMessages}:{responseMessages:Array<CoreToolMessage | CoreAssistantMessage>} ) => {
       if (session.user?.id) {
 
           const responseMessagesWithoutIncompleteToolCalls = sanitizeResponseMessages(responseMessages);
